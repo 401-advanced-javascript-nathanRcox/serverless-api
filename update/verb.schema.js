8135617a0ba0eea8eaedfa3d291942f53cb8d4f4
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-30 22:16:16 -0800

    Working on update, but update has yet to work. (Delete is beyond the horizon.)

---
+++ update/verb.schema.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const dynamoose = require('dynamoose');
+
+const verbSchema = new dynamoose.Schema({
+  'id': String,
+  'infinitive': String,
+  'singular': { // Verb conjugations for я, ты, он/а/о.
+    'type': Array,
+    'schema': [String]
+  },
+  'plural': { // Verb conjugations for мы, вы, они.
+    'type': Array,
+    'schema': [String]
+  },
+  'aspect': String,
+  'ofMotion': { type: Boolean, default: false }
+});
+
+module.exports = dynamoose.model('verb', verbSchema);
